196ba544ff17a4734aecef3e5aea17a74020c47a
Anatoly Zapravadov <zapokami@gmail.com>
2023-12-27 16:23:06 +0300

    basic methods done

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ class Server:
         self.connected_clients = {}
 
     async def register(self, reader, writer):
-        # Получаем данные от клиента
+        """
+        Метод регистрации на сервере.
+        Содаёт объект user с username и password
+        Вызывает метод создания клиента с базовым параметрами.
+        """
         writer.write('input your username:'.encode())
         data = await reader.read(1024)
         username = data.decode()
@@ -34,6 +38,24 @@ class Server:
             await writer.drain()
         except sqlite3.OperationalError as e:
             writer.write(f"Database data corrupted, {e}".encode())
+
+    async def _add_client(self, writer, ram, cpu, hdd_capacity, user_id):
+        """Метод записи клиента в базу данных"""
+        try:
+            self.cursor.execute("""INSERT INTO clients (ram, cpu, user_id)
+                                VALUES (?, ?, ?)""",
+                                (ram, cpu, user_id))
+            client_id = self.cursor.lastrowid
+            self.cursor.execute("""INSERT INTO disks (capacity, client_id)
+                                VALUES (?, ?)""",
+                                (hdd_capacity, client_id))
+            self.connection.commit()
+            # Отправляем клиенту подтверждение
+            print("Client added to database")
+            await writer.drain()
+        except sqlite3.OperationalError as e:
+            writer.write(f"Client data corrupted, {e}".encode())
+            await writer.drain()
 
     async def login(self, reader, writer, addr):
         """Метод аутентификации клиента."""
@@ -69,35 +91,29 @@ class Server:
         except sqlite3.OperationalError as e:
             writer.write(f"Database data corrupted, {e}".encode())
 
-    async def _add_client(self, writer, ram, cpu, hdd_capacity, user_id):
-        """Метод записи клиента в базу данных"""
-        try:
-            self.cursor.execute("""INSERT INTO clients (ram, cpu, user_id)
-                                VALUES (?, ?, ?)""",
-                                (ram, cpu, user_id))
-            client_id = self.cursor.lastrowid
-            self.cursor.execute("""INSERT INTO disks (capacity, client_id)
-                                VALUES (?, ?)""",
-                                (hdd_capacity, client_id))
-            self.connection.commit()
-            # Отправляем клиенту подтверждение
-            print("Client added to database")
-            await writer.drain()
-        except sqlite3.OperationalError as e:
-            writer.write(f"Client data corrupted, {e}".encode())
-            await writer.drain()
-
     async def update_client(
-            self, reader, writer, ram, cpu, hdd_capacity, user_id):
+            self, reader, writer, addr):
         # Обновляем данные авторизованного клиента в базе данных
+        user_id = self.connected_clients.get(addr)['id']
+        self.cursor.execute(
+            f"""SELECT id from clients WHERE user_id = {user_id}""")
+        client_id = cursor.fetchone()[0]
+
+        writer.write('What parameters to update?\nformat: param=value, '
+                     'param=value\nAccepted params: ram, cpu, '
+                     'capacity'.encode())
+        data = await reader.read(1024)
+        params = data.decode().replace(' ', '').split(',')
+        clients_data = ', '.join(param for param in params
+                                 if 'ram' in param or 'cpu' in param)
+        disk_data = ', '.join(param for param in params if 'capacity' in param)
         try:
-            self.cursor.execute("""UPDATE clients SET ram = ?, cpu = ?,
-                                WHERE id = ?""",
-                                (ram, cpu, user_id))
-            client_id = self.cursor.lastrowid
-            self.cursor.execute("""UPDATE disks (capacity,
-                                user_id) VALUES (?, ?) WHERE client_id = ?""",
-                                (hdd_capacity, client_id))
+            if clients_data:
+                self.connection.execute(f"UPDATE clients SET {clients_data} "
+                                        "WHERE id = '{client_id}'")
+            if disk_data:
+                self.connection.execute(f"UPDATE disks SET {disk_data}"
+                                        f"WHERE id = '{client_id}'")
             self.connection.commit()
             writer.write("Client data updated".encode())
             await writer.drain()
@@ -106,16 +122,26 @@ class Server:
             await writer.drain()
         # Отправляем клиенту подтверждение
 
-    async def get_all_clients(self, reader, writer):
-        # Получаем список всех клиентов из базы данных
-        self.cursor.execute("""SELECT * FROM clients JOIN disks
-                    ON clients.id=disks.client_id""")
-        clients = self.cursor.fetchall()
-
+    async def get_all_clients(self, writer):
+        """Метод получения всех подключенных клиентов из базы данных"""
+        self.cursor.execute("""SELECT users.id, users.username,
+                                clients.ram, clients.cpu, disks.id,
+                                disks.capacity
+                                FROM clients
+                                JOIN disks on clients.id = disks.client_id
+                                JOIN users ON clients.user_id = users.id""")
+        db_clients = self.cursor.fetchall()
+        clients = [{
+                    'user_id': db_client[0],
+                    'username': db_client[1],
+                    'ram': db_client[2],
+                    'cpu': db_client[3],
+                    'hdd_id': db_client[4],
+                    'hdd_capacity': db_client[5]
+                } for db_client in db_clients]
         # Отправляем список клиентов клиенту
-        for client in clients:
-            writer.write(str(client).encode())
-            await writer.drain()
+        writer.write(str(clients).encode())
+        await writer.drain()
 
     async def get_connected_clients(self, writer, authorized=False):
         """Метод получения всех подключенных клиентов
@@ -130,9 +156,9 @@ class Server:
             else:
                 suitable_clients = self.connected_clients
 
-            clients_ids = ", ".join([f"\'{a['id']}\'"
-                                    for a in suitable_clients.values()
-                                    if a.get('id')])
+            clients_ids = ", ".join(f"'{data['id']}'"
+                                    for data in suitable_clients.values()
+                                    if data.get('id'))
             self.cursor.execute(f"""SELECT users.id, users.username,
                                 clients.ram, clients.cpu, disks.id,
                                 disks.capacity
@@ -144,14 +170,14 @@ class Server:
             db_clients = self.cursor.fetchall()
             clients = []
             for socket, data in suitable_clients.items():
-                if data.get('id'):
+                if connected_id := data.get('id'):
                     for db_client in db_clients:
-                        if data['id'] == db_client[0]:
-                            user_socket = socket
+                        if connected_id == db_client[0]:
                             clients.append(
                                 {
-                                    'socket': user_socket,
+                                    'socket': socket,
                                     'authorized': data['authorized'],
+                                    'user_id': db_client[0],
                                     'username': db_client[1],
                                     'ram': db_client[2],
                                     'cpu': db_client[3],
@@ -163,7 +189,7 @@ class Server:
                 else:
                     clients.append(
                         {
-                            'socket': user_socket,
+                            'socket': socket,
                             'authorized': data['authorized'],
                         }
                     )
@@ -173,17 +199,27 @@ class Server:
             writer.write(f"Client data corrupted, {e}".encode())
             await writer.drain()
 
-    async def get_all_disks(self, reader, writer):
-        # Получаем список всех жестких дисков из базы данных
-        self.cursor.execute("SELECT * FROM disks")
-        disks = self.cursor.fetchall()
-
+    async def get_all_disks(self, writer):
+        """Метод получения всех жёстких дисков с параметрами клиентов"""
+        self.cursor.execute("""SELECT disks.id, disks.capacity,
+                            disks.client_id, clients.ram, clients.cpu,
+                            clients.user_id
+                            FROM disks
+                            JOIN clients ON disks.client_id = clients.id""")
+        db_disks = self.cursor.fetchall()
+        disks = [{
+                'disk_id': db_disk[0],
+                'disk_capacity': db_disk[1],
+                'client_id': db_disk[2],
+                'ram': db_disk[3],
+                'cpu': db_disk[4],
+                'user_id': db_disk[5]
+                } for db_disk in db_disks]
         # Отправляем список жестких дисков клиенту
-        for disk in disks:
-            writer.write(str(disk).encode())
-            await writer.drain()
+        writer.write(str(disks).encode())
+        await writer.drain()
 
-    async def quit(self, reader, writer, addr):
+    async def quit(self, writer, addr):
         """Метод выхода клиента с сервера."""
         writer.close()
         self.connected_clients.pop(addr)
@@ -207,12 +243,13 @@ class Server:
             try:
                 if not self.connected_clients[addr]['authorized']:
                     match message:
+                        # Методы, достпные неавторизованному клиенту
                         case 'register':
                             await self.register(reader, writer)
                         case "login":
                             await self.login(reader, writer, addr)
                         case "quit":
-                            await quit(reader, writer, addr)
+                            await quit(writer)
                         case ("get_all_clients" | "get_authorized_clients" |
                               "get_all_connected_clients" | "get_all_disks"):
                             writer.write(
@@ -223,19 +260,22 @@ class Server:
                             writer.write('Unrecognised command!'.encode())
                 else:
                     match message:
+                        # Методы, достпные авторизованному клиенту
                         case "get_all_clients":
-                            await self.get_all_clients(reader, writer, addr)
+                            await self.get_all_clients(writer)
 
                         case "get_authorized_clients":
-                            await self.get_authorized_clients(writer, authorized=True)
-                        case "get_all_connected_clients":
-                            await self.get_authorized_clients(writer)
-                        # case "update_client":
-                        #     await self.update_client(reader, writer)
+                            await self.get_connected_clients(
+                                writer, authorized=True
+                            )
+                        case "get_connected_clients":
+                            await self.get_connected_clients(writer)
+                        case "update_client":
+                            await self.update_client(reader, writer, addr)
                         case "get_all_disks":
-                            await self.get_all_disks(reader, writer)
+                            await self.get_all_disks(writer)
                         case "quit":
-                            await quit(writer, addr)
+                            await quit(writer)
                         case _:
                             writer.write('Unrecognised command!'.encode())
             except ConnectionError:
